d18fa9e708ff3d6fc894f724f2b21adb653b2305
nandita2102 <nandita1512ys@gmail.com>
2022-12-05 02:39:11 +0530

    CTA section component complete

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './cta.css'
 
+// CTA : Click To Action/Get Started
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gpt3_cta'>
+      <div className='gpt3_cta_content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='gpt3_cta_button'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
