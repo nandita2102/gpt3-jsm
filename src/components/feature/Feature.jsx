bd13fd061b27a4909296c702b48478d4a504ad54
nandita2102 <nandita1512ys@gmail.com>
2022-12-04 20:26:27 +0530

    WhatGPT3 and Feature components complete

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import './feature.css'
+import React from 'react';
+import './feature.css';
 
-const Feature = () => {
+const Feature = ({title, text}) => {
   return (
-    <div>Feature</div>
+    <div className='gpt3_features_container_feature'>
+      <div className='gpt3_features_container_feature_title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3_features_container_feature_text'>
+        <p>{text}</p>
+      </div>
+    </div>
   )
 }
 
