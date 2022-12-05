1fe4b2321c131a53f19c8a9f09af55f9067b457e
nandita2102 <nandita1512ys@gmail.com>
2022-12-05 21:30:57 +0530

    Blog and Article section components complete

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl, date, title}) => {
   return (
-    <div>Article</div>
+    <div className='gpt3_blog_container_article'>
+      <div className='gpt3_blog_container_article_image'>
+        <img src={imgUrl} alt='blog' />
+      </div>
+      <div className='gpt3_blog_container_article_content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import './blog.css'
+import React from 'react';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+import './blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3_blog section_padding'>
+      <div className='gpt3_blog_heading'>
+        <h1 className='gradient_text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3_blog_container'>
+        <div className='gpt3_blog_container_groupA'>
+          <Article imgUrl={blog01} date='Dec 5, 2022' title='GPT-3 and Open  AI is the future. Let us exlore how it is?'/>
+        </div>
+        <div className='gpt3_blog_container_groupB'>
+          <Article imgUrl={blog02} date='Dec 5, 2022' title='GPT-3 and Open  AI is the future. Let us exlore how it is?'/>
+          <Article imgUrl={blog03} date='Dec 5, 2022' title='GPT-3 and Open  AI is the future. Let us exlore how it is?'/>
+          <Article imgUrl={blog04} date='Dec 5, 2022' title='GPT-3 and Open  AI is the future. Let us exlore how it is?'/>
+          <Article imgUrl={blog05} date='Dec 5, 2022' title='GPT-3 and Open  AI is the future. Let us exlore how it is?'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
